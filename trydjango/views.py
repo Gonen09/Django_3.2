fedaf3cd9f53eca74a59b003862171828ebcca3b
Gonen09 <gonen.rt@gmail.com>
2023-08-02 15:55:22 -0400

    creada primera vista simple

---
+++ trydjango/views.py
@@ -0,0 +1,20 @@
+"""
+To render html web pages
+"""
+from django.http import HttpResponse
+
+
+def home_view(request):
+    """
+    Take in a request (Django sends request)
+    Return HTML as a response (We pick to return the response)
+    """
+    name = "Gonen"
+
+    HTML_STRING = f"""
+
+        <h1>Hello {name} !!!</h1>
+
+    """
+
+    return HttpResponse(HTML_STRING)
