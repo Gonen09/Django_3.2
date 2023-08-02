97309fc9a3de4793d28e8777ac2a91b00146dbd6
Gonen09 <gonen.rt@gmail.com>
2023-08-02 19:52:53 -0400

    actualizada la forma de cargar los datos desde la vista principal

--- trydjango/views.py
+++ trydjango/views.py
@@ -2,35 +2,24 @@
 To render html web pages
 """
 from django.http import HttpResponse
+from django.template.loader import render_to_string
 from articles.models import Article
 
 
-def home_view(request):
+def home_view(request, *args, **kwargs):
     """
     Take in a request (Django sends request)
     Return HTML as a response (We pick to return the response)
     """
 
     article_obj = Article.objects.get(id=2)
+    article_queryset = Article.objects.all()
+
+    # Metodo 1
 
     # article_title = article_obj.title
     # article_content = article_obj.content
     # article_id = article_obj.id
-
-    context = {
-
-        "id": article_obj.id,
-        "title": article_obj.title,
-        "content": article_obj.content
-    }
-
-    name = "Gonen"
-
-    HTML_STRING = f"""
-
-        <h1>Hello {name} !!!</h1>
-
-    """
 
     # HTML_DATA = f"""
     #    <h3>{article_title}</h3>
@@ -38,12 +27,32 @@ def home_view(request):
     #    <p>{article_id}</p>
     # """
 
-    HTML_DATA = """
-        <h3>{title}</h3>
-        <p>{content}</p>
-        <p>{id}</p>
-    """.format(**context)
+    # Metodo 2
+    # context = {
+#
+    #    "id": article_obj.id,
+    #    "title": article_obj.title,
+    #    "content": article_obj.content
+    # }
+#
+    # HTML_DATA = """
+    #    <h3>{title}</h3>
+    #    <p>{content}</p>
+    #    <p>{id}</p>
+    # """.format(**context)
 
-    HTML_RESPONSE = HTML_STRING + HTML_DATA
+    # Metodo 3
+
+    context = {
+        "object_list": article_queryset,
+        "objet": article_obj,
+        "id": article_obj.id,
+        "title": article_obj.title,
+        "content": article_obj.content
+    }
+
+    # HTML_RESPONSE = HTML_STRING + HTML_DATA
+
+    HTML_RESPONSE = render_to_string("home-view.html", context=context)
 
     return HttpResponse(HTML_RESPONSE)
