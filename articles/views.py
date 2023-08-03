4bb0eb81cedef514be6a8b5b9b205e5194ff9265
Gonen09 <gonen.rt@gmail.com>
2023-08-03 17:05:31 -0400

    creada busqueda basica en formulario

--- articles/views.py
+++ articles/views.py
@@ -4,6 +4,25 @@ from .models import Article
 # Create your views here.
 
 
+def article_search_view(request):
+
+    query_dict = request.GET  # dictionay
+
+    try:
+        query = query_dict.get("q")  # < input type='text' name='q' />
+    except:
+        query = None
+
+    article_obj = None
+
+    if query is not None:
+        article_obj = Article.objects.get(id=query)
+
+    context = {"object": article_obj}
+
+    return render(request, "articles/search.html", context=context)
+
+
 def article_detail_view(request, id=None):
 
     article_obj = Article.objects.get(id=id) if id is not None else None
