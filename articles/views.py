df767c97dda2c6962a6ad0c196685eec9b5a52b4
Gonen09 <gonen.rt@gmail.com>
2023-08-09 20:30:14 -0400

    creada la vista para crear articulos

--- articles/views.py
+++ articles/views.py
@@ -23,6 +23,23 @@ def article_search_view(request):
     return render(request, "articles/search.html", context=context)
 
 
+def article_create_view(request):
+
+    context = {}
+
+    if request.method == "POST":
+
+        print(request.POST)
+        title = request.POST.get("title")
+        content = request.POST.get("content")
+        print(title, content)
+        article_object = Article.objects.create(title=title, content=content)
+        context['object'] = article_object
+        context['created'] = True
+
+    return render(request, "articles/create.html", context=context)
+
+
 def article_detail_view(request, id=None):
 
     article_obj = Article.objects.get(id=id) if id is not None else None
