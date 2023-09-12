307accec05f697909a39f18142448a17c38b47bb
Gonen09 <gonen.rt@gmail.com>
2023-09-12 20:41:37 -0300

    agregada url absoluta para acceder a los articulos

--- articles/models.py
+++ articles/models.py
@@ -16,6 +16,9 @@ class Article(models.Model):
     publish = models.DateTimeField(
         auto_now_add=False, auto_now=False, default=timezone.now)
 
+    def get_absolute_url(self):
+        return f'/articles/{self.slug}/'
+
     def save(self, *args, **kwargs):
 
         # if self.slug is None:
